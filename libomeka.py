5674d10ecb12957d8d21b6d45330e6d39132ec02
nakamura196 <na.kamura.1263@gmail.com>
2022-03-30 09:21:33 +0900

    feat: initial commit

---
+++ libomeka.py
@@ -0,0 +1,75 @@
+import json
+import requests
+import os
+from lib import Curation
+
+class Omeka:
+    api = ""
+    key = ""
+    output_dir = ""
+
+    types = [
+        "collections", 
+        "element_sets", 
+        "elements", 
+        "files", 
+        "item_types",
+        "tags",
+        # "users"
+    ]
+
+    def loadEnv(self, file):
+        with open(file) as f:
+            env = json.load(f)
+
+        self.api = env["api"]
+        self.key = env["key"]
+        self.output_dir = env["output_dir"]
+
+    def downloadAllResources(self):
+        for type in self.types:
+            self.downloadResources(type)
+    
+    def downloadResources(self, type="items"):
+        api_url = self.api
+        key = self.key
+
+        loop_flg = True
+        page = 1
+
+        dir = "{}/api/{}".format(self.output_dir, type)
+        os.makedirs(dir, exist_ok=True)
+
+        while loop_flg:
+            url = "{}/{}?page={}".format(api_url, type, page)
+
+            if key != "":
+                url += "&key="+key
+
+            print(url)
+
+            page += 1
+
+            headers = {"content-type": "application/json"}
+            r = requests.get(url, headers=headers)
+            data = r.json()
+
+            if len(data) > 0:
+                for i in range(len(data)):
+                    obj = data[i]
+
+                    id = obj["id"]
+
+                    uri = "{}/{}.json".format(api_url, id)
+
+                    obj["@id"] = uri
+
+                    with open("{}/{}.json".format(dir, id), 'w') as outfile:
+                        json.dump(obj, outfile, ensure_ascii=False,
+                                indent=4, sort_keys=True, separators=(',', ': '))
+
+            else:
+                loop_flg = False
+
+    def createCuration(self):
+        Curation.create(self.output_dir)
---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+ 
+setuptools.setup(
+    name="libomeka",
+    version="1.0",
+    author="nakamura196",
+    author_email="na.kamura.1263@gmail.com",
+    description="IIIFと機械学習に関するライブラリです。",
+    long_description="IIIFと機械学習に関するライブラリです。そして、...。",
+    long_description_content_type="text/markdown",
+    url="https://github.com/ldasjp8/libiiif",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
